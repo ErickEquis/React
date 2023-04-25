e1ed74f77c38807325b5868d9b4008ac3b2286b8
Erick <erick.cfeteit@gmail.com>
2023-04-25 10:24:10 -0600

    Se actualiza App.js con Hook en numClics

--- contador-clicks/src/App.js
+++ contador-clicks/src/App.js
@@ -4,15 +4,22 @@ import './stylesheets/Contador.css'
 import Boton from './componentes/boton';
 import hellfishLogo from './imagenes/hellfish.png'
 import Contador from './componentes/contador';
+// Hook: Permite hacer funcionales a los estados
+import { useState } from 'react';
 
 function App() {
 
+  // Se crea array con var. para el estado y funcion que le dira que hacer.
+  // useState le dice en que estado iniciara
+  const[numClics, setNumClics] = useState(0) 
+
   const manejarClic = () => {
-    console.log("Clic")
+    // La funcion setNumClics se le dice que valor le pasara al estado de numClics
+    setNumClics(numClics + 1)
   }
 
   const reiniciarContador = () => {
-    console.log("Reiniciar")
+    setNumClics(0)
   }
 
   return (
@@ -24,7 +31,7 @@ function App() {
         alt='Logo de contenedor'/>
       </div>
       <div className='contador-principal'>
-        <Contador numClics={'666'}/>
+        <Contador numClics={numClics}/>
         <Boton
           texto='Clic'
           esBotonClic={true} 
